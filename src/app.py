e01551498a9601efabf1e0f0ca52e2dd752916a1
justPipes <datathings@proton.me>
2025-06-03 17:52:06 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -83,4 +83,5 @@ def handle_exception(e):
     ''''
     '''
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True, port=5000,threaded=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0',debug=True, port=port,threaded=True)
